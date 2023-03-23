29e069e7dff22a48105a79deb09470369531c7e8
AngelahMbula <mbula.angelah@gmail.com>
2023-03-23 11:21:16 +0000

    updates

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,14 +1,9 @@
-export function getFullResponseFromAPI(success) {
-	return new Promise((resolve, reject) => {
-		if (success == true) {
-			const response = {
-				status: 200,
-				body: 'Success'
-			};
-			resolve(response);
-		} else {
-			const error = new Error('The fake API is not working currently');
-			reject(error);
-		}
-	});
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      resolve('status: 200 "\n" body: "Success"');
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
 }
