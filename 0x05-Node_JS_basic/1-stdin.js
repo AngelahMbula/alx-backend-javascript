9b879acf30825e7abb70c9c5d09cc07507e5841b
Angelah Mbula Maweu <104361772+AngelahMbula@users.noreply.github.com>
2023-07-17 15:46:12 +0300

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,14 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rL = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
-console.log('Welcome to Holberton School, what is your name?');
-
-rL.on('line', (input) => {
-  console.log(`Your name is: ${input}`);
-  rL.close();
-});
-
-process.on('exit', () => {
-  console.log('This important software is now closing');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
